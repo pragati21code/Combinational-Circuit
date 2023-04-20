3a6dbc65535fc35f1ae52e3819776710c87f3ad6
pragati21code <126967333+pragati21code@users.noreply.github.com>
2023-04-21 00:35:48 +0530

    Create test_bench.v

---
+++ Full_adder/Behivoral/test_bench.v
@@ -0,0 +1,39 @@
+
+module full_addar_tb();
+reg a,b,c;
+wire s,cy;
+full_addar_beh dut(.a(a),.b(b),.c(c),.s(s),.cy(cy));
+initial begin
+a = 1'b0;
+b = 1'b0;
+c = 1'b0;
+#20
+a = 1'b0;
+b = 1'b0;
+c = 1'b1;
+#20
+a = 1'b0;
+b = 1'b1;
+c = 1'b0;
+#20;
+a = 1'b0;
+b = 1'b1;
+c = 1'b1;
+#20;
+a = 1'b1;
+b = 1'b0;
+c = 1'b0;
+#20
+a = 1'b1;
+b = 1'b0;
+c = 1'b1;
+#20
+a = 1'b1;
+b = 1'b1;
+c = 1'b0;
+#20
+a = 1'b1;
+b = 1'b1;
+c = 1'b1;
+end
+endmodule
