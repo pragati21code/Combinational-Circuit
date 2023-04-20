93fbcb7e225db437ae23759d5e82f539a765d719
pragati21code <126967333+pragati21code@users.noreply.github.com>
2023-04-21 00:59:31 +0530

    Create test_bench.v

---
+++ Half Subtractor/Structural/test_bench.v
@@ -0,0 +1,21 @@
+
+module half_sub_tb( );
+reg a,b;
+wire d,br;
+half_sub_structural dut(.a(a) ,.b(b), .d(d) ,.br(br));
+ 
+initial begin;
+a = 1'b0;
+b = 1'b0;
+#20;
+a = 1'b0;
+b = 1'b1;
+#20;
+a = 1'b1;
+b = 1'b0;
+#20;
+a = 1'b1;
+b = 1'b1;
+#20;
+end;
+endmodule
