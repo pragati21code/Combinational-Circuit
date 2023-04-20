d06a610320d3bf57e58384a6a4836e2e861789fe
pragati21code <126967333+pragati21code@users.noreply.github.com>
2023-04-21 00:33:21 +0530

    Create test_bench.v

---
+++ Decoder/Data Flow/test_bench.v
@@ -0,0 +1,19 @@
+
+module decoder_tb(   );
+reg a0,a1;
+wire y0,y1,y2,y3;
+decoder_dataflow dut(.y0(y0),.y1(y1),.y2(y2),.y3(y3),.a0(a0),.a1(a1));
+initial begin
+a0=0;
+a1=0;
+#10
+a0=1;
+a1=0;
+#10
+a0=0;
+a1=1;
+#10
+a0=1;
+a1=1;
+end
+endmodule
