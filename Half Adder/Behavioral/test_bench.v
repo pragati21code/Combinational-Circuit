95382ca872fc7b413b7025c1c10ce331dff6b4d7
pragati21code <126967333+pragati21code@users.noreply.github.com>
2023-04-21 01:01:19 +0530

    Create test_bench.v

---
+++ Half Adder/Behavioral/test_bench.v
@@ -0,0 +1,24 @@
+
+module half_addar_tb(
+
+    );
+    reg a,b;
+    wire s,cy;
+    half_adaar_beh dut(.a(a) ,.b(b), .s(s) ,.cy(cy));
+     
+    initial begin;
+    a = 1'b0;
+    b = 1'b0;
+    #20;
+    a = 1'b0;
+    b = 1'b1;
+    #20;
+    a = 1'b1;
+    b = 1'b0;
+    #20;
+    a = 1'b1;
+    b = 1'b1;
+    
+    end
+
+endmodule
