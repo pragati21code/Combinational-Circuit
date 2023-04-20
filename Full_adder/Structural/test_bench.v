9542a83c74926620a9af1c00b41e6ec7d07f95bd
pragati21code <126967333+pragati21code@users.noreply.github.com>
2023-04-21 00:37:53 +0530

    Create test_bench.v

---
+++ Full_adder/Structural/test_bench.v
@@ -0,0 +1,39 @@
+
+module full_addar_tb( );
+    reg a,b,c;
+    wire s,cy;
+    fulll_addar_struct dut(.a(a),.b(b),.c(c),.s(s),.cy(cy));
+    initial begin
+    a = 1'b0;
+    b = 1'b0;
+    c = 1'b0;
+    #20
+    a = 1'b0;
+    b = 1'b0;
+    c = 1'b1;
+    #20
+    a = 1'b0;
+    b = 1'b1;
+    c = 1'b0;
+    #20;
+    a = 1'b0;
+    b = 1'b1;
+    c = 1'b1;
+    #20;
+    a = 1'b1;
+    b = 1'b0;
+    c = 1'b0;
+    #20
+    a = 1'b1;
+    b = 1'b0;
+    c = 1'b1;
+    #20
+    a = 1'b1;
+    b = 1'b1;
+    c = 1'b0;
+    #20
+    a = 1'b1;
+    b = 1'b1;
+    c = 1'b1;
+    end
+    endmodule
